7cb3a1552cb3924e53bf527b63635fcf6bb51734
Shravang007 <shravangoudgajula@gmail.com>
2023-07-05 22:28:51 +0530

    alb

---
+++ main.tf
@@ -0,0 +1,56 @@
+resource "aws_security_group" "main" {
+  name        = "${var.name}-${var.env}-sg"
+  description = "${var.name}-${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.sg_subnet_cidr
+  }
+
+#  ingress {
+#    from_port   = 443
+#    to_port     = 443
+#    protocol    = "tcp"
+#    cidr_blocks = var.sg_subnet_cidr
+#  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-${var.env}-sg"
+  }
+}
+
+resource "aws_lb" "main" {
+  name               = "${var.name}-${var.env}-lb"
+  internal           = var.internal
+  load_balancer_type = var.load_balancer_type
+  security_groups    = [aws_security_group.main.id]
+  subnets            = var.subnets
+  tags               = merge({ Name = "${var.name}-${var.env}-lb" }, var.tags)
+}
+
+resource "aws_lb_listener" "public" {
+  count             = var.name == "public" ? 1 : 0
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
